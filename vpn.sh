3531443eeb41a054185b672f7f7859f9a923688e
zlmalu <1992123836@qq.com>
2020-08-11 16:13:52 +0800

    vpn

---
+++ vpn.sh
@@ -0,0 +1,12 @@
+yum install -y ppp
+rpm -ivh http://static.ucloud.cn/pptpd-1.3.4-2.el6.x86_64.rpm
+echo localip 10.8.0.1 >> /etc/pptpd.conf
+echo remoteip 10.8.0.10-100 >> /etc/pptpd.conf
+echo ms-dns >> 8.8.8.8
+echo ms-dns >> 8.8.4.4
+echo account       pptpd       password       *   >> /etc/ppp/chap-secrets
+echo net.ipv4.ip_forward = 1 >> /etc/sysctl.conf
+sysctl -p
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+iptables-save > /etc/sysconfig/iptables
+systemctl start pptpd.service
