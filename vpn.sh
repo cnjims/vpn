3b41d9f70255e4f90f9d5f29a0887775c2045cb1
zlmalu <1992123836@qq.com>
2020-09-18 10:25:54 +0800

    a

--- vpn.sh
+++ vpn.sh
@@ -2,8 +2,8 @@ yum install -y ppp
 rpm -ivh http://static.ucloud.cn/pptpd-1.3.4-2.el6.x86_64.rpm
 echo localip 10.8.0.1 >> /etc/pptpd.conf
 echo remoteip 10.8.0.10-100 >> /etc/pptpd.conf
-echo ms-dns >> 8.8.8.8
-echo ms-dns >> 8.8.4.4
+echo ms-dns  8.8.8.8 >> /etc/ppp/options.pptpd
+echo ms-dns  8.8.4.4 >> /etc/ppp/options.pptpd
 echo account       pptpd       password       *   >> /etc/ppp/chap-secrets
 echo net.ipv4.ip_forward = 1 >> /etc/sysctl.conf
 sysctl -p
